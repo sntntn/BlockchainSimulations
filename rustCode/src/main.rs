97959d315551935c17ebe28676e64ad344f7e838
Vukasin Markovic <skolskiacc@gmail.com>
2025-04-06 23:57:08 +0200

    Deserialize Ethereum block and transactions into Rust structs and display key fields
    Implemented structured deserialization of Ethereum JSON-RPC response for eth_getBlockByNumber.
    Introduced RPCResponse, SimpleBlock, and SimpleTransaction structs with appropriate Serde annotations (rename_all = "camelCase", etc).
    Added printing of key fields from the latest block and its transactions in a clean format.

--- rustCode/src/main.rs
+++ rustCode/src/main.rs
@@ -1,8 +1,39 @@
 use std::{env, path::Path};
 use dotenvy::from_path;
 use reqwest::Client;
+use serde::Deserialize;
 use serde_json::json;
 use tokio;
+
+
+#[derive(Debug, Deserialize)]
+struct RPCResponse {
+    jsonrpc: String,
+    id: u64,
+    result: SimpleBlock,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SimpleBlock {
+    number: String,
+    hash: String,
+    timestamp: String,
+    gas_used: String,
+    transactions: Vec<SimpleTransaction>,
+}
+
+#[derive(Debug, Deserialize)]
+struct SimpleTransaction {
+    hash: String,
+    from: String,
+    to: Option<String>,
+    value: String,
+    gas: String,
+    
+    #[serde(rename = "gasPrice")]
+    gas_price: String,
+}
 
 
 #[tokio::main]
@@ -28,6 +59,32 @@ async fn main() {
         .await
         .expect("Greska pri slanju zahteva");
 
-    let resp_text = resp.text().await.expect("Greska pri citanju odgovora");
-    println!("Odgovor sa servera:\n{}", resp_text);
+    // let resp_text = resp.text().await.expect("Greska pri citanju odgovora");
+    // println!("Odgovor sa servera:\n{}", resp_text);
+
+    let resp_json: RPCResponse = resp
+        .json()
+        .await
+        .expect("Greska pri parsiranju odgovora");
+
+
+    let block = resp_json.result;
+
+
+    println!("Blok broj: {}", block.number);
+    println!("Hash bloka: {}", block.hash);
+    println!("Timestamp: {}", block.timestamp);
+    println!("Gas used: {}", block.gas_used);
+    println!("Broj transakcija: {}", block.transactions.len());
+
+    for tx in block.transactions {
+        println!("---");
+        println!("Tx hash: {}", tx.hash);
+        println!("From: {}", tx.from);
+        println!("To: {}", tx.to.unwrap_or_else(|| "N/A".into()));
+        println!("Value: {}", tx.value);
+        println!("Gas: {}", tx.gas);
+        println!("Gas price: {}", tx.gas_price);
+    }
+
 }
