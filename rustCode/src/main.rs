2522d7d3f8df4ca9950dda8e45cf882baafab71f
Vukasin Markovic <skolskiacc@gmail.com>
2025-10-07 11:39:52 +0200

    refactor: modularize

--- rustCode/src/main.rs
+++ rustCode/src/main.rs
@@ -1,44 +1,12 @@
 mod config;
 mod models;
 mod rpc;
+mod utils;
 
 use config::{get_testnet_rpc_url, get_mainnet_rpc_url, load_testnet_env};
-use models::SimpleBlock;
-use rpc::fetch::{fetch_latest_block, fetch_block_by_number};
+use rpc::fetch::{fetch_latest_block, fetch_block_by_number, fetch_transaction_receipt};
+use utils::{print_block_info, print_transactions, find_max_gas_transaction};
 use tokio;
-
-use crate::{models::SimpleTransaction, rpc::fetch::fetch_transaction_receipt};
-
-fn print_block_info(block: &SimpleBlock) {
-    println!("Blok broj: {}", block.number);
-    println!("Hash bloka: {}", block.hash);
-    println!("Timestamp: {}", block.timestamp);
-    println!("Gas used: {}", block.gas_used);
-    println!("Broj transakcija: {}", block.transactions.len());
-    println!("Transakcije: ");
-    //print_transactions(&block.transactions);
-}
-
-fn print_transactions(transactions: &[SimpleTransaction]) {
-    for tx in transactions {
-        println!("---");
-        println!("Tx hash: {}", tx.hash);
-        println!("From: {}", tx.from);
-        println!("To: {}", tx.to.clone().unwrap_or_else(|| "N/A".into()));
-        println!("Value: {}", tx.value);
-        println!("Gas: {}", tx.gas);
-        println!("Gas price: {}", tx.gas_price);
-    }
-
-    println!("----------------------");
-    println!("ispisano je {} transakcija", transactions.len());
-}
-
-fn find_max_gas_transaction(transactions: &[SimpleTransaction]) -> Option<&SimpleTransaction> {
-    transactions
-        .iter()
-        .max_by_key(|tx| u64::from_str_radix(tx.gas.trim_start_matches("0x"), 16).unwrap_or(0))
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -81,7 +49,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else {
             println!("Nema transakcija u ovom bloku");
     }
-
 
     Ok(())
 }
