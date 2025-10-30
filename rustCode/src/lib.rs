0036c228e647e8fac25e288a04f5813464ea81f8
Vukasin Markovic <skolskiacc@gmail.com>
2025-10-30 23:28:25 +0100

    minor refactor

--- rustCode/src/lib.rs
+++ rustCode/src/lib.rs
@@ -9,17 +9,14 @@ use crate::utils::hex_to_u64;
 
 #[no_mangle]
 pub extern "C" fn fetch_transactions(rpc_url: *const c_char) -> *mut c_char {
-    // Pretvaranje C stringa u Rust string
     let c_str = unsafe { std::ffi::CStr::from_ptr(rpc_url) };
     let rpc_url_str = c_str.to_str().unwrap();
-    // Pozivanje fetch_latest_block funkcije za dobijanje najnovijeg bloka
-    let future = tokio::runtime::Runtime::new()
-        .unwrap()
-        .block_on(fetch_latest_block(rpc_url_str, true));
 
-    // Prikupljanje transakcija iz rezultata
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    let latest_block = runtime.block_on(fetch_latest_block(rpc_url_str, true));
+
     let mut transaction_data = String::new();
-    for tx in &future.transactions {
+    for tx in &latest_block.transactions {
         transaction_data.push_str(&format!(
             "Tx hash: {}\nFrom: {}\nTo: {}\nValue: {}\nGas: {}\n\n",
             tx.hash,
